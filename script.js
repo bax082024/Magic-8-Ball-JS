3c90f0de552484e57301b6cc5f10f5083c5c6bcb
bax082024 <bax082024@gmail.com>
2024-10-28 22:25:14 +0100

    added js response

---
+++ script.js
@@ -0,0 +1,7 @@
+const response = [
+  "Yes", "No", "Maybe", "Come back later",
+  "Haha! Your funny!", "Go be a painter",
+  "Painterman!!", "Not a chance", "Very unlikely",
+  "Very likely", "Nope", "Go Away!"
+];
+
