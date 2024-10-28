4b52d98cbfc5230936f235553b18091df43cadc5
bax082024 <bax082024@gmail.com>
2024-10-28 22:45:47 +0100

    fixed syntax in js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const response = [
+const responses = [
   "Yes", "No", "Maybe", "Come back later",
-  "Haha! Your funny!", "Go be a painter",
+  "Haha! You're funny!", "Go be a painter",
   "Painterman!!", "Not a chance", "Very unlikely",
   "Very likely", "Nope", "Go Away!"
 ];
 
 function getMagic8BallResponse() {
-  const randomIndex = Math.floor(Math.random() * responses.lenght);
+  const randomIndex = Math.floor(Math.random() * responses.length);
   return responses[randomIndex];
 }
 
 document.getElementById("eightBallImage").addEventListener("click", () => {
   const response = getMagic8BallResponse();
-  document.getElementById("response").textContent = '✨ Magic 8 Ball says: ${response} ✨'
-})
-
+  document.getElementById("response").textContent = `✨ Magic 8 Ball says: ${response} ✨`;
+});
